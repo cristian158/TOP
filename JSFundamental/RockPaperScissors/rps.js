44815320181e9239156c8ae7f47f823e8b19bdeb
Cristian Novoa <cnovoa.o@gmail.com>
2022-02-03 01:18:24 +0300

    minor shiet

--- JSFundamental/RockPaperScissors/rps.js
+++ JSFundamental/RockPaperScissors/rps.js
@@ -1,76 +1,76 @@
 // arrays to keep choices made
-const playerChoice = [];
-const pcChoice = [];
+const playerChoice = []
+const pcChoice = []
 
 // counters to keep score
 // i used const and didn't work
-let playerScore = 0;
-let pcScore = 0;
+let playerScore = 0
+let pcScore = 0
 
 // pc election function
 function computerPlay(){
 	// random return rock paper scissors
 	const array = ['Rock', 'Paper', 'Scissors']
-	const choice = array[Math.floor(Math.random()*3)];
-	pcChoice.push(choice);
-	return choice;
+	const choice = array[Math.floor(Math.random()*3)]
+	pcChoice.push(choice)
+	return choice
 }
 
 // player election function
 function playerPlay(){
-	const weapon = prompt('Choose Weapon');
-	playerChoice.push(weapon);
-	return weapon;
+	const weapon = prompt('Choose Weapon')
+	playerChoice.push(weapon)
+	return weapon
 }
 
 // match
 function playRound(playerSelection, computerSelection){
 	if (playerSelection == 'Rock' && computerSelection == 'Rock'){
-		alert('Tie!');
+		alert('Tie!')
 	} else if (playerSelection == 'Rock' && computerSelection == 'Paper'){
-		alert('PC Wins!');
-		pcScore++;
+		alert('PC Wins!')
+		pcScore++
 	} else if (playerSelection == 'Rock' && computerSelection == 'Scissors'){
-		alert('Player Wins!');
-		playerScore++;
+		alert('Player Wins!')
+		playerScore++
 	} else if (playerSelection == 'Paper' && computerSelection == 'Rock'){
-		alert('Player Wins!!');
-		playerScore++;
+		alert('Player Wins!!')
+		playerScore++
 	} else if (playerSelection == 'Paper' && computerSelection == 'Paper'){
-		alert('Tie!');
+		alert('Tie!')
 	} else if (playerSelection == 'Paper' && computerSelection == 'Scissors'){
-		alert('PC Wins!');
-		pcScore++;
+		alert('PC Wins!')
+		pcScore++
 	} else if (playerSelection == 'Scissors' && computerSelection == 'Rock'){
-		alert('Pc Wins!');
-		pcScore++;
+		alert('Pc Wins!')
+		pcScore++
 	} else if (playerSelection == 'Scissors' && computerSelection == 'Paper'){
-		alert('Player Wins!');
-		playerScore++;
+		alert('Player Wins!')
+		playerScore++
 	} else if (playerSelection == 'Scissors' && computerSelection == 'Scissors'){
-		alert('Tie!');
+		alert('Tie!')
 	} else {
-		alert('Bruh');
+		alert('Bruh')
 	}
 }
 
 function result(){
 	if (playerScore > pcScore){
-		alert('U badass!!');
+		alert('U badass!!')
 	} else if (playerScore < pcScore){
-		alert('Randomness got u.');
+		alert('Randomness got u.')
 	} else {
 		alert('In the end everybody dies')
 	}
 }
 
 
-playRound(computerPlay(), playerPlay());
-playRound(computerPlay(), playerPlay());
-playRound(computerPlay(), playerPlay());
-playRound(computerPlay(), playerPlay());
-playRound(computerPlay(), playerPlay());
-result();
+playRound(computerPlay(), playerPlay())
+playRound(computerPlay(), playerPlay())
+playRound(computerPlay(), playerPlay())
+playRound(computerPlay(), playerPlay())
+playRound(computerPlay(), playerPlay())
+result()
 
 
 ///////////////////////////////////////////////////////////////
